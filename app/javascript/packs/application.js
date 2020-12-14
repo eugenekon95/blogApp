5e5d38dbaab006d3db3fca767fcc240363180ab1
Yevhenii Kononenko <evgenijkononenko0@gmail.com>
2020-12-14 16:42:53 +0200

    refactoring

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,15 +8,6 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-require("jquery")
-require("bootstrap")
-
-document.addEventListener("turbolinks:load", function() {
-    $(function () {
-        $('[data-toggle="tooltip"]').tooltip()
-        $('[data-toggle="popover"]').popover()
-    })
-})
 
 
 
