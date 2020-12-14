a8c987a73852e773e41f6d71dc35aacdfd417472
Yevhenii Kononenko <evgenijkononenko0@gmail.com>
2020-12-14 13:31:46 +0200

    app created

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+    
+    validates :title, presence: true
+    validates :content, presence: true
+    validates :title, length: { maximum: 10 }
+    validates :content, length: { maximum: 500 }
 end
