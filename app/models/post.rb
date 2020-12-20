9d337266992ffcaef3e0710ee124b9eca2b331df
Yevhenii Kononenko <evgenijkononenko0@gmail.com>
2020-12-20 21:39:27 +0200

    reupload

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,9 @@
 class Post < ApplicationRecord
-    
-    belongs_to :author
-    has_many :comments
-    validates :title, presence: true
-    validates :content, presence: true
-    validates :title, length: { maximum: 20 }
-    validates :content, length: { maximum: 1500 }
-end
+  belongs_to :author
+  has_many :comments, dependent: :destroy
 
+  validates :name, presence: true
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :title, length: { maximum: 20, message:  "is too long"}
+end
--- db/migrate/20201219164412_add_author_to_posts.rb
+++
@@ -1,5 +0,0 @@
-class AddAuthorToPosts < ActiveRecord::Migration[6.1]
-  def change
-    add_reference :posts, :author
-  end
-end
