0d359a4478f83cb591b442f2c2ee2b535f2d49b6
Yevhenii Kononenko <evgenijkononenko0@gmail.com>
2020-12-21 14:58:09 +0200

    add publish comment functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,30 @@ class CommentsController < ApplicationController
     redirect_to post_path(@post)
   end
 
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
+
+  def update
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    if @comment.update(comment_params)
+      redirect_to post_path(@post), notice: 'Comment was successfully updated.' 
+    else
+      redirect_to post_path(@post)
+    end
+  end
+
+  def publish
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.update(status: :published)
+    redirect_to post_path(@post), notice: 'Comment was successfully published.' 
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:body, :author_id)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   resources :posts do
-    resources :comments
+    resources :comments do
+      member do
+        post :publish
+      end
+    end
   end
+
+
  
   root  'posts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
