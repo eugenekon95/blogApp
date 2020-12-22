dbe2c5f77a0c276028156deade4f22e9e6e9bc86
Yevhenii Kononenko <evgenijkononenko0@gmail.com>
2020-12-22 16:50:45 +0200

    schema rb update

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_12_20_080540) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,4 +43,5 @@ ActiveRecord::Schema.define(version: 2020_12_20_080540) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "author_id"
   end
+
 end
