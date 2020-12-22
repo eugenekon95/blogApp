745f049de70f3298978a50d5e5fe5387c528e090
Yevhenii Kononenko <evgenijkononenko0@gmail.com>
2020-12-22 15:35:24 +0200

    styling

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,5 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
   validates :title, length: { maximum: 20, message:  "is too long"}
+  validates :content, length: { maximum: 1500, message:  "is too long"}
 end
