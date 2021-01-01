30a3b4a024bd8f1e5d2d3d5195050f54acdc5ece
Yevhenii Kononenko <evgenijkononenko0@gmail.com>
2021-01-01 17:06:47 +0200

    password validation fixed

--- app/models/author.rb
+++ app/models/author.rb
@@ -5,6 +5,7 @@ class Author < ApplicationRecord
   VALID_EMAIL_REGX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :first_name, :last_name, :gender, :birthday, presence: true
   validates :email, presence: true, format: { with: VALID_EMAIL_REGX }
+  validates :password, length: { minimum: 8, message: "is too short for valid password" }
   validates_uniqueness_of :email, case_sensitive: false
   def full_name
     "#{self.first_name} #{self.last_name}"
