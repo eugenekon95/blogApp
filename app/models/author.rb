1e6d2fee64cd9484350c0b906aa8dad600e91a72
Yevhenii Kononenko <evgenijkononenko0@gmail.com>
2021-01-05 08:32:06 +0200

    Fix #7

--- app/models/author.rb
+++ app/models/author.rb
@@ -7,6 +7,14 @@ class Author < ApplicationRecord
   validates :email, presence: true, format: { with: VALID_EMAIL_REGX }
   validates :password, length: { minimum: 8, message: "is too short for valid password" }
   validates_uniqueness_of :email, case_sensitive: false
+  
+  PASSWORD_FORMAT = /\A
+     (?=.{8,})           # Must contain 8 or more characters
+     (?=.*[A-Z])         # Must contain an upper case character
+    (?=.*[[:^alnum:]])  # Must contain a symbol
+    /x
+
+
   def full_name
     "#{self.first_name} #{self.last_name}"
   end
