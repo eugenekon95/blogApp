39a87590ac5a6f442248f15d8d81f83159a56a7b
Yevhenii Kononenko <evgenijkononenko0@gmail.com>
2021-01-06 18:56:23 +0200

    password validation fixed

--- app/models/author.rb
+++ app/models/author.rb
@@ -6,12 +6,17 @@ class Author < ApplicationRecord
   has_many :author_comment_votes
   has_one_attached :avatar
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
 
-  validates :password, presence: true, length: { minimum: 6, maximum: 50 }, unless: :persisted?
-  validates :email, presence: true, length: {
-    minimum: 5, maximum: 50
-  }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+  PASSWORD_FORMAT = /\A
+      (?=.{8,})           # Must contain 8 or more characters
+      (?=.*[A-Z])         # Must contain an upper case character
+      (?=.*[[:^alnum:]])  # Must contain a symbol
+    /x
+  
+    validates :password_digest, presence: true, format: { with: PASSWORD_FORMAT }, unless: :persisted?
+    validates :email, presence: true, length: { minimum: 8, maximum: 50 },format: { with: VALID_EMAIL_REGEX },uniqueness: { case_sensitive: false }
+  
 
   def full_name
     "#{first_name} #{last_name} "
