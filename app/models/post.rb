0c9f8a76fd5a3b5ed64d267909b51d0d76f60403
Yevhenii Kononenko <evgenijkononenko0@gmail.com>
2021-01-06 19:24:33 +0200

    pagination added, UI Fixed

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,7 +8,7 @@ class Post < ApplicationRecord
   validates :content, presence: true
   validates :title, length: { maximum: 20, message: 'is too long' }
   validates :content, length: { maximum: 1500, message: 'is too long' }
-  paginates_per 3
+  paginates_per 1
   #max_paginates_per 8
 
 end
